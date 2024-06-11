fd1ad6026b83ff92ad795373953f8e445a4d42e8
teorhan <teorhn@gmail.com>
2024-06-11 23:32:46 +0300

    Not Page geliştirildi

--- gazimobil/NotPage.xaml.cs
+++ gazimobil/NotPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.Maui.Controls;
 
+
 namespace gazimobil
 {
     public partial class NotPage : ContentPage
@@ -11,6 +12,15 @@ namespace gazimobil
         public NotPage()
         {
             InitializeComponent();
+        }
+
+        private void OnCalculationTypeChanged(object sender, EventArgs e)
+        {
+            bool isSemester = CalculationTypePicker.SelectedIndex == 0;
+            bool isBoth = CalculationTypePicker.SelectedIndex == 1;
+
+            SemesterInputs.IsVisible = isSemester || isBoth;
+            BothInputs.IsVisible = isBoth;
         }
 
         private async void OnAddCourseClicked(object sender, EventArgs e)
@@ -38,7 +48,7 @@ namespace gazimobil
             string gradeLetter = GradePicker.SelectedItem.ToString();
             double grade = GetNumericGrade(gradeLetter);
 
-            Course course = new Course { Name = courseName, Credit = credit, Grade = grade, GradeLetter= gradeLetter };
+            Course course = new Course { Name = courseName, Credit = credit, Grade = grade, GradeLetter = gradeLetter };
             courses.Add(course);
             CoursesListView.ItemsSource = null;
             CoursesListView.ItemsSource = courses;
@@ -53,19 +63,19 @@ namespace gazimobil
 
         private double GetNumericGrade(string gradeLetter)
         {
-            switch (gradeLetter)
+            return gradeLetter switch
             {
-                case "AA": return 4.0;
-                case "BA": return 3.5;
-                case "BB": return 3.0;
-                case "CB": return 2.5;
-                case "CC": return 2.0;
-                case "DC": return 1.5;
-                case "DD": return 1.0;
-                case "FD": return 0.5;
-                case "FF": return 0.0;
-                default: throw new ArgumentException("Geçersiz harf notu!");
-            }
+                "AA" => 4.0,
+                "BA" => 3.5,
+                "BB" => 3.0,
+                "CB" => 2.5,
+                "CC" => 2.0,
+                "DC" => 1.5,
+                "DD" => 1.0,
+                "FD" => 0.5,
+                "FF" => 0.0,
+                _ => throw new ArgumentException("Geçersiz harf notu!"),
+            };
         }
 
         private void UpdateAverages()
@@ -81,21 +91,52 @@ namespace gazimobil
 
             double semesterAverage = totalPoints / totalCredits;
             SemesterAverageLabel.Text = semesterAverage.ToString("F2");
+
+
+        }
+
+        private async void OnCalculateBothGpaClicked(object sender, EventArgs e)
+        {
+            if (!double.TryParse(BothCreditsEntry.Text, out double existingCredits) || existingCredits <= 0)
+            {
+                await DisplayAlert("Hata", "Lütfen geçerli bir mevcut kredi girin.", "Tamam");
+                return;
+            }
+
+            if (!double.TryParse(BothGpaEntry.Text, out double existingGpa) || existingGpa < 0 || existingGpa > 4)
+            {
+                await DisplayAlert("Hata", "Lütfen geçerli bir mevcut ortalama girin.", "Tamam");
+                return;
+            }
+
+            double totalPoints = existingGpa * existingCredits;
+            double totalCredits = existingCredits;
+
+            foreach (var course in courses)
+            {
+                totalPoints += course.Credit * course.Grade;
+                totalCredits += course.Credit;
+            }
+
+            double overallGpa = totalPoints / totalCredits;
+            BothOverallGpaLabel.Text = overallGpa.ToString("F2");
+
+            UpdateAverages();
         }
 
         private void OnDeleteCourse(object sender, EventArgs e)
         {
             var menuItem = sender as MenuItem;
             var course = menuItem?.BindingContext as Course;
-                if (course != null)
-                {
-                    courses.Remove(course);
-                    CoursesListView.ItemsSource = null;
-                    CoursesListView.ItemsSource = courses;
-                    UpdateAverages();
-                }
+            if (course != null)
+            {
+                courses.Remove(course);
+                CoursesListView.ItemsSource = null;
+                CoursesListView.ItemsSource = courses;
+                UpdateAverages();
             }
         }
+    }
 
     public class Course
     {
@@ -110,3 +151,4 @@ namespace gazimobil
         }
     }
 }
+
