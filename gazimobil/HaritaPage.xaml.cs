58a48884c71761430ebdc0b68660abe567326104
Ahmet Balkan <ahmetbalkangulyyew@gmail.com>
2024-06-12 10:18:26 +0300

    harita turkce

--- gazimobil/HaritaPage.xaml.cs
+++ gazimobil/HaritaPage.xaml.cs
@@ -5,10 +5,10 @@ namespace gazimobil
 {
     public partial class HaritaPage : ContentPage
     {
-        double currentScale = 1;
-        double startScale = 1;
-        double xOffset = 0;
-        double yOffset = 0;
+        double mevcutOlcek = 1;
+        double baslangicOlcek = 1;
+        double xKaydirma = 0;
+        double yKaydirma = 0;
 
         public HaritaPage()
         {
@@ -20,43 +20,37 @@ namespace gazimobil
         {
             if (e.Status == GestureStatus.Started)
             {
-                startScale = mapImage.Scale;
-                mapImage.AnchorX = 0;
-                mapImage.AnchorY = 0;
+                baslangicOlcek = haritaResmi.Scale;
+                haritaResmi.AnchorX = 0;
+                haritaResmi.AnchorY = 0;
             }
             if (e.Status == GestureStatus.Running)
             {
-                // Calculate the scale factor to be applied.
-                currentScale = startScale * e.Scale;
-                currentScale = Math.Max(1, currentScale);
+                mevcutOlcek = baslangicOlcek * e.Scale;
+                mevcutOlcek = Math.Max(1, mevcutOlcek);
 
-                // The ScaleOrigin is in relative coordinates to the wrapped user interface element.
-                double renderedX = mapImage.X + xOffset;
-                double deltaX = renderedX / Width;
-                double deltaWidth = Width / (mapImage.Width * startScale);
-                double originX = (e.ScaleOrigin.X - deltaX) * deltaWidth;
+                double renderX = haritaResmi.X + xKaydirma;
+                double deltaX = renderX / Width;
+                double deltaWidth = Width / (haritaResmi.Width * baslangicOlcek);
+                double orijinX = (e.ScaleOrigin.X - deltaX) * deltaWidth;
 
-                double renderedY = mapImage.Y + yOffset;
-                double deltaY = renderedY / Height;
-                double deltaHeight = Height / (mapImage.Height * startScale);
-                double originY = (e.ScaleOrigin.Y - deltaY) * deltaHeight;
+                double renderY = haritaResmi.Y + yKaydirma;
+                double deltaY = renderY / Height;
+                double deltaHeight = Height / (haritaResmi.Height * baslangicOlcek);
+                double orijinY = (e.ScaleOrigin.Y - deltaY) * deltaHeight;
 
-                // Calculate the transformed element bounds.
-                double targetX = xOffset - (originX * mapImage.Width) * (currentScale - startScale);
-                double targetY = yOffset - (originY * mapImage.Height) * (currentScale - startScale);
+                double hedefX = xKaydirma - (orijinX * haritaResmi.Width) * (mevcutOlcek - baslangicOlcek);
+                double hedefY = yKaydirma - (orijinY * haritaResmi.Height) * (mevcutOlcek - baslangicOlcek);
 
-                // Apply translation based on the change in origin.
-                mapImage.TranslationX = Math.Min(0, Math.Max(targetX, -mapImage.Width * (currentScale - 1)));
-                mapImage.TranslationY = Math.Min(0, Math.Max(targetY, -mapImage.Height * (currentScale - 1)));
+                haritaResmi.TranslationX = Math.Min(0, Math.Max(hedefX, -haritaResmi.Width * (mevcutOlcek - 1)));
+                haritaResmi.TranslationY = Math.Min(0, Math.Max(hedefY, -haritaResmi.Height * (mevcutOlcek - 1)));
 
-                // Apply scale factor.
-                mapImage.Scale = currentScale;
+                haritaResmi.Scale = mevcutOlcek;
             }
             if (e.Status == GestureStatus.Completed)
             {
-                // Store the current translation applied during the pinch gesture.
-                xOffset = mapImage.TranslationX;
-                yOffset = mapImage.TranslationY;
+                xKaydirma = haritaResmi.TranslationX;
+                yKaydirma = haritaResmi.TranslationY;
             }
         }
 
@@ -65,28 +59,28 @@ namespace gazimobil
             switch (e.StatusType)
             {
                 case GestureStatus.Running:
-                    // Translate and ensure we don't pan beyond the wrapped user interface element bounds.
-                    double newX = (e.TotalX * currentScale) + xOffset;
-                    double newY = (e.TotalY * currentScale) + yOffset;
+             
+                    double yeniX = (e.TotalX * mevcutOlcek) + xKaydirma;
+                    double yeniY = (e.TotalY * mevcutOlcek) + yKaydirma;
 
-                    double maxX = mapImage.Width * (currentScale - 1);
-                    double maxY = mapImage.Height * (currentScale - 1);
+                    double maxX = haritaResmi.Width * (mevcutOlcek - 1);
+                    double maxY = haritaResmi.Height * (mevcutOlcek - 1);
 
-                    mapImage.TranslationX = Math.Min(0, Math.Max(newX, -maxX));
-                    mapImage.TranslationY = Math.Min(0, Math.Max(newY, -maxY));
+                    haritaResmi.TranslationX = Math.Min(0, Math.Max(yeniX, -maxX));
+                    haritaResmi.TranslationY = Math.Min(0, Math.Max(yeniY, -maxY));
                     break;
 
                 case GestureStatus.Completed:
-                    // Store the translation applied during the pan gesture.
-                    xOffset = mapImage.TranslationX;
-                    yOffset = mapImage.TranslationY;
+                    
+                    xKaydirma = haritaResmi.TranslationX;
+                    yKaydirma = haritaResmi.TranslationY;
                     break;
             }
         }
 
         private async void HaritaButtonClicked(object sender, EventArgs e)
         {
-            var url = "https://www.google.com/maps/place/Gazi+Üniversitesi/@39.9396355,32.8032515,15z/data=!4m10!1m2!2m1!1sGazi+Üniversitesi!3m6!1s0x14d34eda2bd572e3:0xc4944b9ae7b9927!8m2!3d39.9396355!4d32.8223059!15sChJHYXppIMOcbml2ZXJzaXRlc2kiA4gBAZIBEXB1YmxpY191bml2ZXJzaXR54AEA!16zL20vMDdoNjM4?hl=tr&entry=ttu";
+            var url = "https://www.google.com/maps/place/Gazi+Üniversitesi/@39.9396355,32.8032515,15z/data=!4m10!1m2!2m1!1sGazi+Üniversitesi!3m6!1s0x14d34eda2bd572e3:0xc4944b9ae7b9927!8m2!3d39.9396355!4d32.8223059!15sChJHYXppIMOcbml2ZXJzaXRlc2kiA4gBAZIBEXB1YmxpY191bml2ZXJzaXR54AEA!16zL20vMDhoNjM4?hl=tr&entry=ttu";
             await Navigation.PushAsync(new WebViewPage(url));
         }
     }
